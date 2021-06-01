0cb37c3084d15630bd49f8a126976aa1033ba460
largemable <mable.palombo@gmail.com>
2021-06-01 15:05:46 -0500

    Initial commit

---
+++ finsta/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class FinstaEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField()
+    caption = models.TextField()
+    image = models.ImageField(null=True)
+
+    def __str__(self):
+        return self.title
---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE finsta;
+CREATE USER finstauser WITH PASSWORD 'finsta';
+GRANT ALL PRIVILEGES ON DATABASE finsta TO finstauser;
