a24da84f23b1f6fcad69efa433849ab958c20a22
largemable <mable.palombo@gmail.com>
2021-06-08 21:30:21 -0500

    add user auth

--- finsta_project/settings.py
+++ finsta_project/settings.py
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
                                                                                  ],
 
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 # Internationalization
