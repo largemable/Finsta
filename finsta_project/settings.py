3c1d9c756e08c6b50a4f1646818345f7b8f3f79a
largemable <mable.palombo@gmail.com>
2021-06-09 19:45:53 -0500

    add cloudinary storage - restore some static file settings

--- finsta_project/settings.py
+++ finsta_project/settings.py
@@ -152,14 +152,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
-)
-STATICFILES_DIRS = [
-    f'{BASE_DIR}/static/'
-]
+# STATIC_URL = '/static/'
+# STATICFILES_FINDERS = (
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+# )
+# STATICFILES_DIRS = [
+#     f'{BASE_DIR}/static/'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
